9e691885283a553aa55c1eb9e03937fc3c7cd4df
Lizzie <leejihye7117@gmail.com>
2022-10-27 18:03:14 +0900

    JS:navbarMenuClick

--- main.js
+++ main.js
@@ -20,3 +20,16 @@ document.addEventListener("scroll", () => {
     navbarMenuItem.classList.remove("navbarMenuItem--white");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(e.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
