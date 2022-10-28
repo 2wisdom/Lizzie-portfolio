847bec5198cd333ceccf3dcbb6f3b5ed7e9c5656
Lizzie <leejihye7117@gmail.com>
2022-10-28 14:52:10 +0900

    JS:home contact

--- main.js
+++ main.js
@@ -29,7 +29,16 @@ navbarMenu.addEventListener("click", (e) => {
   if (link == null) {
     return;
   }
-  console.log(e.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", (e) => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
